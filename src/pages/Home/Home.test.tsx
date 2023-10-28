2c07f1c48d4d5bb1189bc6114512d326aab41a5e
Cyrano75 <berthierjg@gmail.com>
2023-10-28 18:43:16 +0200

    add unit test for custom calendar

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -3,13 +3,14 @@ import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import HomePage from "./Home";
 import { renderWithProviders } from "../../utils/utils-for-tests";
+import Slide from "../../models/Slide";
 
 describe("HomePage component", () => {
   test("renders nous contacter page", async () => {
     const routes = [
       {
         path: "/",
-        element: <HomePage />
+        element: <HomePage />,
       },
     ];
 
@@ -21,8 +22,35 @@ describe("HomePage component", () => {
     //https://www.freecodecamp.org/news/how-to-write-unit-tests-in-react-redux/#-how-to-perform-testing-with-the-react-redux-toolkit
     renderWithProviders(<RouterProvider router={router} />);
 
-    const outputElement = screen.getByText("C'est par ici, venez.", {selector: 'a'})
+    const outputElement = screen.getByText("C'est par ici, venez.", {
+      selector: "a",
+    });
     expect(outputElement).toBeInTheDocument();
-    expect(outputElement.getAttribute("href")).toBe("/nous_contacter")
+    expect(outputElement.getAttribute("href")).toBe("/nous_contacter");
+  });
+  test("renders nous contacter page", async () => {
+    const routes = [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/"],
+    });
+
+    const toCheck = "my custom data";
+    renderWithProviders(<RouterProvider router={router} />, {
+      preloadedState: {
+        user: {
+          slides: [],
+          calendar: [toCheck],
+        },
+      },
+    });
+
+    const outputElement = screen.getByText(toCheck);
+    expect(outputElement).toBeInTheDocument();
   });
 });
